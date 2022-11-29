14015eaff736337217fa4e27ee97252aa223ea37
Samson Oana <samson_oana.andreea@yahoo.com>
2022-11-29 15:02:21 +0200

    Laborator7

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,30 @@
-﻿namespace Samson_Oana_Lab7;
+﻿using System;
+using Samson_Oana_Lab7.Data;
+using System.IO;
+
+namespace Samson_Oana_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+
+
+
+    public App()
 	{
 		InitializeComponent();
 
